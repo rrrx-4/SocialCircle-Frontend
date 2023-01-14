5d2703daed8ac11cd2154b54322e25e823380814
rrrx-4 <69399883+rrrx-4@users.noreply.github.com>
2023-01-14 21:15:09 +0530

    raf

--- src/scenes/loginPage/Form.jsx
+++ src/scenes/loginPage/Form.jsx
@@ -64,7 +64,7 @@ const Form = () => {
     formData.append("picturePath", values.picture.name);
 
     const savedUserResponse = await fetch(
-      `${REACT_APP_BASEURL}/auth/register`,
+      `https://socialcircle-server-48bl.onrender.com/auth/register`,
       {
         method: "POST",
         body: formData,
@@ -79,7 +79,7 @@ const Form = () => {
   };
 
   const login = async (values, onSubmitProps) => {
-    const loggedInResponse = await fetch(`${REACT_APP_BASEURL}/auth/login`, {
+    const loggedInResponse = await fetch(`https://socialcircle-server-48bl.onrender.com/auth/login`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(values),
